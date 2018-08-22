d1b1d208d530863b69d33348165c9b6ebfa75c13
Nazif Zuhrić <nazif.zuhric@appimpact.ba>
2018-08-22 17:56:03 +0200

    added web pack

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class App extends Comment {
+    render() {
+        return (
+            <div>
+                <h1>Test</h1>
+            </div>
+        )
+    }
+}
+export default App;
